9eeecc959317ca434275263b26f5208482c416cd
Mark Mathis <markmathis@gmail.com>
2018-01-04 15:03:34 -0500

    stopping point for medium article - todo add error handling

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -1,32 +1,74 @@
 pragma solidity ^0.4.8;
   contract Bet {
 
-    //bet input - originator pays the gas to start the bet
-    uint betAmount = 0;
+    //jedi bet status
+    uint constant STATUS_WIN = 1;
+    uint constant STATUS_LOSE = 2;
+    uint constant STATUS_TIE = 3;
+    uint constant STATUS_PENDING = 4;
+
+    //game status
+    uint constant STATUS_NOT_STARTED = 1;
+    uint constant STATUS_STARTED = 2;
+    uint constant STATUS_COMPLETE = 3;
+
+    //general status
+    uint constant STATUS_ERROR = 4;
+
+    //the 'game' structure
+    struct Game {
+      uint betAmount;
+      uint outcome;
+      uint status;
+    }
 
     //the 'better' structure
     struct JediBet {
       uint guess;
       address addr;
+      uint status;
     }
 
     //the two 'betters'
     JediBet originator;
     JediBet taker;
 
+    //the game
+    Game game;
+
     function createBet(uint _betAmount, uint _guess) public payable {
-      betAmount = _betAmount;
-      originator = JediBet(_guess, msg.sender);
+      game = Game(_betAmount, 0, STATUS_STARTED);
+      originator = JediBet(_guess, msg.sender, STATUS_PENDING);
     }
 
-    function takeBet(uint _betAmount, uint _guess) public payable returns (string){
-      if(_betAmount == betAmount) {
-        taker = JediBet(_guess, msg.sender);
+    function takeBet(uint _betAmount, uint _guess) public payable{
+      if(_betAmount == game.betAmount) {
+        taker = JediBet(_guess, msg.sender, STATUS_PENDING);
+        generateBetOutcome();
       }
     }
 
+    function payout() public payable {
+
+     if(originator.status == STATUS_TIE && taker.status == STATUS_TIE) {
+       originator.addr.transfer(game.betAmount);
+       taker.addr.transfer(game.betAmount);
+     } else {
+        if(originator.status == STATUS_WIN) {
+          originator.addr.transfer(game.betAmount*2);
+        }
+        else if(taker.status == STATUS_WIN) {
+          taker.addr.transfer(game.betAmount*2);
+        }
+        else {
+          originator.addr.transfer(game.betAmount);
+          taker.addr.transfer(game.betAmount);
+        }
+     }
+   }
+
     function getBetAmount() public view returns (uint) {
-      return betAmount;
+      return game.betAmount;
     }
 
      function getOriginatorGuess() public view returns (uint) {
@@ -41,29 +83,58 @@ pragma solidity ^0.4.8;
         return this.balance;
      }
 
-     function getBetOutcome() public view returns (string) {
-
+    function generateBetOutcome() private {
         //todo - not a great way to generate a random number but ok for now
-        uint outcome = uint(block.blockhash(block.number-1))%10 + 1;
+        game.outcome = uint(block.blockhash(block.number-1))%10 + 1;
+        game.status = STATUS_COMPLETE;
 
         if(originator.guess == taker.guess) {
-          return 'Both bets were the same, the pot will be split';
-        } else if(originator.guess > outcome && taker.guess  > outcome) {
-          return 'Both bets were greater than the number so the pot will be split';
+          originator.status = STATUS_TIE;
+          taker.status = STATUS_TIE;
+        } else if(originator.guess > game.outcome && taker.guess  > game.outcome) {
+          originator.status = STATUS_TIE;
+          taker.status = STATUS_TIE;
         } else {
-           if(originator.guess == outcome) {
-             return 'Bet originator guessed the number and will receive twice the pot';
-           } else if(taker.guess == outcome) {
-            return 'Bet taker guessed the number and will receive twice the pot';
-           } else if((outcome - originator.guess) < (outcome - taker.guess)) {
-             return 'Bet originator guess was closer to the number and will receive the pot';
+           if((game.outcome - originator.guess) < (game.outcome - taker.guess)) {
+             originator.status = STATUS_WIN;
+             taker.status = STATUS_LOSE;
            }
-           else if((outcome - taker.guess) < (outcome - originator.guess)) {
-             return 'Bet taker guess was closer to the number and will receive the pot';
+           else if((game.outcome - taker.guess) < (game.outcome - originator.guess)) {
+             originator.status = STATUS_LOSE;
+             taker.status = STATUS_WIN;
            }
            else {
-             return '';
+             originator.status = STATUS_ERROR;
+             taker.status = STATUS_ERROR;
+             game.status = STATUS_ERROR;
            }
         }
-      }
+    }
+
+     //returns - [<description>, 'originator', <originator status>, 'taker', <taker status>]
+     function getBetOutcome() public view returns
+     (string description,
+      string originatorKey,
+      uint originatorStatus,
+      string takerKey,
+      uint takerStatus) {
+
+        if(originator.status == STATUS_TIE || taker.status == STATUS_TIE) {
+          description = 'Both bets were the same or were over the number, the pot will be split';
+        } else {
+           if(originator.status == STATUS_WIN) {
+             description = 'Bet originator guess was closer to the number and will receive the pot';
+           }
+           else if(taker.status == STATUS_WIN) {
+             description = 'Bet taker guess was closer to the number and will receive the pot';
+           }
+           else {
+             description = 'Unknown Bet Outcome';
+           }
+        }
+        originatorKey = 'originator';
+        originatorStatus = originator.status;
+        takerKey = 'taker';
+        takerStatus = taker.status;
+     }
   }
--- test/testBet.js
+++ test/testBet.js
@@ -1,28 +1,36 @@
 var Bet = artifacts.require('./contracts/Bet');
 
-const agreedUponBetAmount = 1; //eth
+const betAmountInEth = 0.25;
+const agreedUponBetAmount = web3.toWei(betAmountInEth, 'ether');
 
 contract('Bet', function(accounts) {
   const betOriginator = accounts[0];
   const betTaker = accounts[1];
-  const originatorBet = 5;
-  const takerBet = 4;
+  const originatorBet = 4;
+  const takerBet = 5;
 
-  let originatorBalance = web3.eth.getBalance(betOriginator);
-  let takerBalance = web3.eth.getBalance(betTaker);
+  const originatorBalanceBeforeBet = web3.eth.getBalance(betOriginator);
+  const takerBalanceBeforeBet = web3.eth.getBalance(betTaker);
+  let originatorBalanceAfterBet;
+  let takerBalanceAfterBet;
 
   it('We should be able to create a bet by setting an agreed upon amount and guess', function() {
     return Bet.deployed().then(function(instance) {
-      return instance.createBet.sendTransaction(agreedUponBetAmount, originatorBet, {
-        from: betOriginator,
-        value: web3.toWei(agreedUponBetAmount, 'ether'),
-      });
+      return instance.createBet
+        .sendTransaction(agreedUponBetAmount, originatorBet, {
+          from: betOriginator,
+          value: agreedUponBetAmount,
+        })
+        .then(tx => {
+          assert.notEqual(tx, '', 'We should get a transaction hash');
+        });
     });
   });
 
   it('The originating bet amount in the contract should match the passed in values', function() {
     return Bet.deployed().then(function(instance) {
       return instance.getBetAmount().then(betAmount => {
+        console.log('betAmount: ' + agreedUponBetAmount);
         assert.equal(betAmount, agreedUponBetAmount, "Bet amounts don't match");
       });
     });
@@ -36,18 +44,26 @@ contract('Bet', function(accounts) {
     });
   });
 
-  it('The originator balance should be less the bet amount', function() {
-    const remainingBalance = originatorBalance - web3.toWei(agreedUponBetAmount, 'ether');
-    const currentBalance = web3.eth.getBalance(betOriginator);
-    assert.equal(currentBalance.toString(), remainingBalance.toString(), "Balances aren't adding up");
+  it('The originator balance should be less the bet amount and gas', function() {
+    const originalBalanceMinusBet = originatorBalanceBeforeBet - agreedUponBetAmount;
+    originatorBalanceAfterBet = web3.eth.getBalance(betOriginator);
+    assert.equal(
+      originatorBalanceAfterBet < originalBalanceMinusBet,
+      true,
+      'Current Balance should be less than original balance minus bet because of gas',
+    );
   });
 
   it('We should be able to take a bet by setting an agreed upon amount and guess', function() {
     return Bet.deployed().then(function(instance) {
-      return instance.takeBet.sendTransaction(agreedUponBetAmount, takerBet, {
-        from: betTaker,
-        value: web3.toWei(agreedUponBetAmount, 'ether'),
-      });
+      return instance.takeBet
+        .sendTransaction(agreedUponBetAmount, takerBet, {
+          from: betTaker,
+          value: agreedUponBetAmount,
+        })
+        .then(tx => {
+          assert.notEqual(tx, '', 'We should get a transaction hash');
+        });
     });
   });
 
@@ -59,11 +75,21 @@ contract('Bet', function(accounts) {
     });
   });
 
+  it('The taker balance should be less the bet amount and gas', function() {
+    const originalBalanceMinusBet = takerBalanceBeforeBet - agreedUponBetAmount;
+    takerBalanceAfterBet = web3.eth.getBalance(betTaker);
+    assert.equal(
+      takerBalanceAfterBet < originalBalanceMinusBet,
+      true,
+      'Current Balance should be less than original balance minus bet because of gas',
+    );
+  });
+
   it('The contract balance should reflect the originator and taker bets', function() {
     return Bet.deployed().then(function(instance) {
       return instance.getPot().then(balance => {
         assert.equal(
-          web3.fromWei(balance.toString(), 'ether'),
+          balance.toString(),
           (agreedUponBetAmount * 2).toString(),
           'Contact Balance should equal the bet amounts ',
         );
@@ -71,11 +97,64 @@ contract('Bet', function(accounts) {
     });
   });
 
-  it('The bet outcome should be available', function() {
+  it('The taker or originator should be able to call the payout to transfer winnings', function() {
+    return Bet.deployed().then(function(instance) {
+      return instance.payout().then(tx => {
+        assert.notEqual(tx.tx, '', 'We should get a transaction hash');
+      });
+    });
+  });
+
+  it('Originator and Taker balances should reflect bet outcome', function() {
     return Bet.deployed().then(function(instance) {
       return instance.getBetOutcome().then(outcome => {
-        console.log(outcome);
-        assert.notEqual(outcome, '', 'Bet outcome should not be empty');
+        assert.notEqual(outcome[0], '', 'Bet outcome description should not be empty');
+        assert.notEqual(outcome[2], '', 'Bet originator status should not be empty');
+        assert.notEqual(outcome[4], '', 'Bet taker status should not be empty');
+
+        const originatorBalanceAfterPayout = web3.eth.getBalance(betOriginator);
+        const takerBalanceAfterPayout = web3.eth.getBalance(betTaker);
+
+        console.log(JSON.stringify(outcome));
+
+        if (outcome[2].toString() === '1') {
+          let gain = originatorBalanceAfterPayout.minus(originatorBalanceBeforeBet);
+          console.log('originator gain:' + gain);
+
+          //if originator won
+          assert.equal(
+            gain.dividedBy(agreedUponBetAmount).greaterThan(0.9),
+            true,
+            'Balance Gain after payout for a winning bet should be within 10% of bet amount',
+          );
+        } else if (outcome[4].toString() === '1') {
+          let gain = takerBalanceAfterPayout.minus(takerBalanceBeforeBet);
+          console.log('taker gain:' + gain);
+
+          //if taker won
+          assert.equal(
+            gain.dividedBy(agreedUponBetAmount).greaterThan(0.9),
+            true,
+            'Balance Gain after payout for a winning bet should be within 10% of bet amount',
+          );
+        } else {
+          //a tie or error
+
+          let takerDelta = takerBalanceBeforeBet.minus(takerBalanceAfterPayout).dividedBy(takerBalanceBeforeBet);
+
+          let originatorDelta = originatorBalanceBeforeBet
+            .minus(originatorBalanceAfterPayout)
+            .dividedBy(originatorBalanceBeforeBet);
+
+          console.log('originatorDelta: ' + originatorDelta);
+          console.log('takerDelta: ' + takerDelta);
+
+          assert.equal(
+            takerDelta.lessThan(0.01) && originatorDelta.lessThan(0.01),
+            true,
+            'Balance after payout for a tied bet should be within 1% of original balance',
+          );
+        }
       });
     });
   });
